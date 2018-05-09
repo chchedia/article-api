6b938138c79f6edcac145fa1a83ddd871642bc8e
chchedia <chebichedia@gmail.com>
2018-05-09 23:16:31 +0200

    Create a Form using ArticleType and deserialize data.

--- src/AppBundle/Controller/ArticleController.php
+++ src/AppBundle/Controller/ArticleController.php
@@ -3,11 +3,15 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Article;
-use FOS\RestBundle\Controller\FOSRestController;
+//use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations\Get;
 use FOS\RestBundle\Controller\Annotations\Post;
 use FOS\RestBundle\Controller\Annotations\View;
+use FOS\RestBundle\Controller\FOSRestController;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Form\ArticleType;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
@@ -34,10 +38,14 @@ class ArticleController extends FOSRestController
      *     name= "app_article_create"
      * )
      * @View(StatusCode= 201)
-     * @ParamConverter("article", converter="fos_rest.request_body")
      */
-    public function createAction(Article $article)
+    public function createAction(Request $request)
     {
+        $data = $this->get('jms_serializer')->deserialize($request->getContent(), 'array', 'json');
+        $article = new Article();
+        $form = $this->get('form.factory')->create(ArticleType::class, $article);
+        $form->submit($data);
+
         $em=$this->getDoctrine()->getManager();
         $em->persist($article);
         $em->flush();
