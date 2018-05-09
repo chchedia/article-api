43957704780de5e8a25c2433bd086e51499e1ae8
chchedia <chebichedia@gmail.com>
2018-05-09 22:42:05 +0200

    Desrialisation with paramConverter.

--- src/AppBundle/Controller/ArticleController.php
+++ src/AppBundle/Controller/ArticleController.php
@@ -3,11 +3,16 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Article;
+use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations\Get;
+use FOS\RestBundle\Controller\Annotations\Post;
 use FOS\RestBundle\Controller\Annotations\View;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 
-class ArticleController
+class ArticleController extends FOSRestController
 {
     /**
      * @Get(
@@ -17,11 +22,29 @@ class ArticleController
      *     )
      * @View
      */
-    public function showAction()
+    public function showAction(Article $article)
     {
-        $article = new Article();
-        $article->setTitle("hola !");
-        $article->setContent("bOnjou!! je suis là!");
-        return $article;
+        $data = $this->get('jms_serializer')->serialize($article, 'json');
+        $response = new Response($data);
+        return $response;
+    }
+    /**
+     * @Post(
+     *     path= "/articles",
+     *     name= "app_article_create"
+     * )
+     * @View(StatusCode= 201)
+     * @ParamConverter("article", converter="fos_rest.request_body")
+     */
+    public function createAction(Article $article)
+    {
+        $em=$this->getDoctrine()->getManager();
+        $em->persist($article);
+        $em->flush();
+
+        return $this->view($article, Response::HTTP_CREATED,
+            ['Location' => $this->generateUrl('app_article_show',
+                ['id' => $article->getId(),
+                    UrlGeneratorInterface::ABSOLUTE_URL])]);
     }
 }
